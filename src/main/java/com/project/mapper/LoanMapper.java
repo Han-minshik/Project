fd4cc517480ecd93e948e95d67efaaacc58217f3
Han-minshik <hanminshik0@naver.com>
2025-01-11 21:03:24 +0900

    2025-01-11
    추가
    - AdminService
    - LoanService
    - 한 책은 50번의 대출을 생성할 수 있음. (조건)
    - 한 사람의 대출은 5권을 초과할 수 없음.(조건)
    - loan-mapper, LoanService, loan-mapper.xml : 대출 가능한 책 재고 확인, 특정 책의 재고 감소, 증가
    - 반납 날짜 = loan_date + 7
    - UserRestService
    - UserService
    - BookService
    
    수정
    - admin-mapper.xml : insertBook category 추가
    - user-mapper.xml : insertReview bookIsbn 조회 쿼리 변경
    - loan-mapper.xml : createLoan : return_date(loan_date + 7), status = '대출 중'으로 변경, updateloanStatus 쿼리문 변경, getLoansByUserId 모든 컬럼 조회에서 조건 조회로 변경, getAllLoans를 모든 사용자의 대출 기록 조회에서 대출 중인 모든 사용자 조회로 변경
    - loan-mapper : updateloanStatus에서 id와 status를 param으로 받아오는 것으로 변경, createLoan 컬럼 값 DEFAULT로 변경
    - Book DB copies_avaliable DEFAULT 50으로 수정
    
    삭제
    - LoanMapper, loan-mapper.xml : getLoanById 삭제

--- src/main/java/com/project/mapper/LoanMapper.java
+++ src/main/java/com/project/mapper/LoanMapper.java
@@ -2,14 +2,20 @@ package com.project.mapper;
 
 import com.project.dto.LoanDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface LoanMapper {
     void createLoan(LoanDTO loan);
-    LoanDTO getLoanById(int id);
     List<LoanDTO> getLoansByUserId(String userId);
-    void updateLoanStatus(LoanDTO loan);
+    void updateLoanStatus(@Param("id") int id, @Param("status") String status);
     List<LoanDTO> getAllLoans();
+    // 한 사용자가 빌릴 수 있는 책의 권수
+    int getActiveLoanCountByUserId(@Param("userId") String userId);
+    // 모든 사용자가 한 책에 대해 빌릴 수 있는 대출 횟
+    int getAvailableCopies(@Param("bookIsbn") int bookIsbn);
+    void decreaseCopiesAvailable(@Param("bookIsbn") int bookIsbn);
+    void increaseCopiesAvailable(@Param("bookIsbn") int bookIsbn);
 }
