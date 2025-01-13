f157581fa708b21fbd53e4150507f5d1e6963720
Han-minshik <hanminshik0@naver.com>
2025-01-13 14:18:17 +0900

    2025-01-13 admin-mapper.xml : 관리자 등록 생성 AdminMapper : 관리자 등록 생성 및 오타 수정 AdminService : 관리자 특정 로직 개발 및 관리자 권한 부여 및 특정 기능 일반 사용자 제한 configuration.xml : alias 추가 user-mapper.xml : resultType 수정 및 유저 등록을 일반 사용자로 수정

--- src/main/java/com/project/service/AdminService.java
+++ src/main/java/com/project/service/AdminService.java
@@ -1,7 +1,10 @@
 package com.project.service;
 
+import com.project.dto.AdminPostDTO;
 import com.project.dto.BookDTO;
+import com.project.dto.UserDTO;
 import com.project.mapper.AdminMapper;
+import com.project.mapper.UserMapper;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,11 +13,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class AdminService {
     @Autowired private AdminMapper adminMapper;
-    public void add_book(BookDTO book) {
+    @Autowired private UserMapper userMapper;
+
+    private boolean isAdmin(String userId) {
+        UserDTO user = userMapper.getUserById(userId);
+        return user != null && "관리자".equals(user.getRole());
+    }
+
+    public void addBook(String adminId, BookDTO book) {
+        if(!isAdmin(adminId)) {
+            throw new SecurityException("관리자 권한이 없습니다.");
+        }
+
         adminMapper.insertBook(book);
-        log.info("책을 DB에 삽입함.");
+        log.info("책을 DB에 삽입했습니다 : {}", book.getTitle());
 
         adminMapper.insertBookImages(book);
-        log.info("책의 이미지(를)을 DB에 삽입함.");
+        log.info("책의 이미지를 DB에 삽입했습니다. : {}", book.getTitle());
+    }
+
+    public void addAdminPost(String adminId, AdminPostDTO adminPost) {
+        if(!isAdmin(adminId)) {
+            throw new SecurityException("관리자 권한이 없습니다.");
+        }
+        adminMapper.createAdminPost(adminPost);
+        log.info("공지사항을 DB에 삽입했습니다 : {}", adminPost.getTitle());
     }
 }
