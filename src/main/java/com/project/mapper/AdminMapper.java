d43ef0056916b177461307b334668e4425a3996a
Han-minshik <hanminshik0@naver.com>
2025-01-13 14:15:18 +0900

    2025-01-13

--- src/main/java/com/project/mapper/AdminMapper.java
+++ src/main/java/com/project/mapper/AdminMapper.java
@@ -2,16 +2,18 @@ package com.project.mapper;
 
 import com.project.dto.AdminPostDTO;
 import com.project.dto.BookDTO;
+import com.project.dto.UserDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface AdminMapper {
+    void createAdmin(UserDTO admin);
     void insertBook(BookDTO book);
     void insertBookImages(BookDTO book);
 
-    void createdAdminPost(AdminPostDTO adminPost);
+    void createAdminPost(AdminPostDTO adminPost);
     AdminPostDTO getAdminPostById(Integer id);
     List<AdminPostDTO> getAllAdminPosts();
     void updateAdminPost(AdminPostDTO adminPost);
