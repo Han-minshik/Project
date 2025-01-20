ee26839f6ced769e90aaf7c9ca5eada1f62611b0
Han-minshik <hanminshik0@naver.com>
2025-01-20 18:48:37 +0900

    2025-01-20 viewer 로직 추가 DTO-DB 연계 수정 xml 쿼리 수정 css, html 수정 admin 수정된 회원 조회 추가

--- src/main/java/com/project/mapper/AdminMapper.java
+++ src/main/java/com/project/mapper/AdminMapper.java
@@ -9,13 +9,14 @@ import java.util.List;
 
 @Mapper
 public interface AdminMapper {
-    void createAdmin(UserDTO admin);
+    void createAdmin(String userId);
     void insertBook(BookDTO book);
     void updateBook(BookDTO book);
     void deleteBook(String isbn); // Integer → String
     void createAdminPost(AdminPostDTO adminPost);
-    AdminPostDTO getAdminPostById(Integer id);
+    List<AdminPostDTO> getAdminPostById(Integer id);
     List<AdminPostDTO> getAllAdminPosts();
     void updateAdminPost(AdminPostDTO adminPost);
     void deleteAdminPost(Integer id);
+    List<UserDTO> getUpdatedUser();
 }
