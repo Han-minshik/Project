f8059b2c23c054675f59f6bb602f2c40f32eee26
97Kcm <148042427+97Kcm@users.noreply.github.com>
2025-01-31 21:01:11 +0900

    discussion/category · 토론 글쓰기 버튼 클릭시 이동 · 제목으로 검색했을 때 최근 댓글 반환
    (완료)
    
    discussion/discussionId
    · 리뷰 불러오기
    · 리뷰 작성하기
    · 리뷰 좋아요 싫어요(유저 당 댓글에 좋아요나 싫어요를 하나만 가능)
    
    · 리뷰 페이지네이션(구현 필요)

--- target/classes/static/js/main/home.js
+++ target/classes/static/js/main/home.js
@@ -50,3 +50,54 @@ document.addEventListener("DOMContentLoaded", function () {
     rollUpBtn.addEventListener("click", scroll);
 });
 
+function includeHTML(callback) {
+    var z, i, elmnt, file, xhr;
+    /*loop through a collection of all HTML elements:*/
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("include-html");
+        //console.log(file);
+        if (file) {
+            /*make an HTTP request using the attribute value as the file name:*/
+            xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        elmnt.innerHTML = this.responseText;
+                    }
+                    if (this.status == 404) {
+                        elmnt.innerHTML = "Page not found.";
+                    }
+                    /*remove the attribute, and call this function once more:*/
+                    elmnt.removeAttribute("include-html");
+                    includeHTML(callback);
+                }
+            };
+            xhr.open("GET", file, true);
+            xhr.send();
+            /*exit the function:*/
+            return;
+        }
+    }
+    setTimeout(function() {
+        callback();
+    }, 0);
+}
+
+    // header와 footer를 불러오는 함수
+    function loadHTML(elementId, file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById(elementId).innerHTML = html;
+        })
+        .catch(err => console.warn('HTML 로딩 오류:', err));
+}
+
+    // 페이지 로드 시 header와 footer를 불러오기
+    window.onload = function() {
+    loadHTML('header-container', '/header');  // 서버에서 반환된 HTML 삽입
+    loadHTML('footer-container', '/footer');  // 서버에서 반환된 HTML 삽입
+};
